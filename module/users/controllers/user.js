7fc6143cf7e765c87f1dda2454f1aa62aa1ac107
Divine <divinetidy@gmail.com>
2025-06-02 23:03:52 +0100

    added get user api

--- module/users/controllers/user.js
+++ module/users/controllers/user.js
@@ -2,12 +2,15 @@ const mongoose = require("mongoose");
 
 const user = async (req, res) => {
   const usersModel = mongoose.model("users");
-  const { email } = req.body;
-  const user = await usersModel.findOne({ email: email });
+  const { email } = req.query;
+  const foundUser = await usersModel.findOne({ email:email });
+  if (!foundUser) {
+    return res.status(404).json({ message: "User not found" });
+  }
   res.status(200).json({
     message: "successsful",
     status: true,
-    data: user.first_Name,
+    data: foundUser.first_Name,
   });
 };
 
